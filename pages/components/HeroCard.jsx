c0919e39e9274502d8e9129af67fe0602d02a430
Isaac Jordan kimsey <jordankimsey@gmail.com>
2022-06-22 17:00:43 -0400

    fixed image path url bug

--- pages/components/HeroCard.jsx
+++ pages/components/HeroCard.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
-
-
+import Image from 'next/image';
 
 const HeroCard = ({ title, description, imagePath }) => {
-
-    return (
-    <Box sx={{margin: 2, border: 'none'}}>
-      <img src={`../../images/${imagePath}`} width={65} alt={title}/>
-      <Typography variant='h4' sx={{fontWeight: 400}}>{title}</Typography>
-      <Typography sx={{pt: 2}} variant='body1' color='neutral.main'>{description}</Typography>
+  const imagepathSlug = `/../images/${imagePath}`;
+  return (
+    <Box sx={{ margin: 2, border: 'none' }}>
+      <Image src={imagepathSlug} width={65} height={65} alt={title} />
+      <Typography variant='h4' sx={{ fontWeight: 400 }}>
+        {title}
+      </Typography>
+      <Typography sx={{ pt: 2 }} variant='body1' color='neutral.main'>
+        {description}
+      </Typography>
     </Box>
   );
 };
